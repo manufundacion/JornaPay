88658331014222c948020015893b8314a84d7a09
manufundacion <58770157+manufundacion@users.noreply.github.com>
2025-05-08 09:46:59 +0200

    Cerrar sesion realizado

---
+++ JornaPay/ViewModels/CerrarSesion.cs
@@ -0,0 +1,16 @@
+﻿namespace JornaPay.Pages;
+using Microsoft.Maui.Controls;
+
+public partial class CerrarSesion : ContentPage 
+{
+    public CerrarSesion()
+    {
+        CerrarSesionYRedirigir();
+    }
+
+    private void CerrarSesionYRedirigir()
+    {
+        // Redirijo a InicioRegistro
+        Application.Current.MainPage = new NavigationPage(new InicioRegistro());
+    }
+}
